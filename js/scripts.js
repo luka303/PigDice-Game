16d75bc1765ebf0678044c05c584a7b2c56d194c
luka wandanje <lukawandanje@gmail.com>
2019-04-08 08:15:26 +0300

    adds more code to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,31 @@ function playing() {
 
 }
 playing.prototype.roll1 = function () {
-    if this.roll === 1 {
+    if (this.roll === 1) {
         this.score = 0;
         alert("TOO BAD!YOUU ROLLED A ONE!!")
     }else {
         this.score+=this.roll;
     }
 }
-//
+//to hold
+player.prototype.hold = function() {
+    this.total += this.score;
+    this.score + 0;
+    alert("next player");
+    if (this.score = 100) {
+        alert(!!!"YOU WIN!!!!!")
+    }
+}
+
+//user interface
+$(document).ready(function () {
+    player1 = new player();
+    player2 = new player();
+    $("button#lava").click(function () {
+      player1.roll = rollDice();
+      $("#roll1").text(player1.roll)
+      player1.rollOne();
+      $("#sum").text(player1.turnScore);
+    });
+});
